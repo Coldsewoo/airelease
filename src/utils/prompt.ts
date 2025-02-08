795e37201bc5a399e9ada5aa61be253953475161
coldsewoo <coldsewoo@gmail.com>
2025-02-09 00:49:32 +0900

    refactor(prompt): update prompt instructions for clarity

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -2,9 +2,11 @@ export const generatePrompt = (locale: string = "English") => {
   return [
     "Given the following list of commit messages, categorize and generate a release message with sections from prefix or content of commit message",
     `Message language: ${locale}`,
-    "Use bullet points in an unordered list format and attach type as prefix for each list (e.g. feature, bug fix, refactor, etc.)",
+    "Add the type as a prefix for each item (e.g., 'Feature', 'Fix', 'QAQC', 'Chore', 'Improve', 'Hotfix' etc.) if not already included.",
+    "Use bullet points in an unordered list format",
     "You must group commit messages by type prefix. and attach header for each group",
-    "Exclude anything unnecessary such as translation. Make response as text only without markdown syntax",
+    "Exclude anything unnecessary such as translation. Make response as text only without markdown syntax, do not drop suffix like (#100)",
+    "Do not generate release notes",
   ]
     .filter(Boolean)
     .join("\n");
