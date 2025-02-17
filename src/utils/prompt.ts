1890acd799aec3f7310a83aac09777fc9e3efd55
coldsewoo <coldsewoo@gmail.com>
2025-02-17 18:52:36 +0900

    refactor(prompt): clean up prompt message instructions

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -3,9 +3,10 @@ export const generatePrompt = (locale: string = "English") => {
     "Given the following list of commit messages, categorize and generate a release message with sections from prefix or content of commit message",
     `Message language: ${locale}`,
     "Add the type as a prefix for each item (e.g., 'Feature', 'Fix', 'QAQC', 'Chore', 'Improve', 'Hotfix' etc.) if not already included.",
+    "Cleanup and sanitize the message content without markdown syntax",
     "Use bullet points in an unordered list format",
     "You must group commit messages by type prefix. and attach header for each group",
-    "Exclude anything unnecessary such as translation. Make response as text only without markdown syntax, do not drop suffix like (#100)",
+    "Exclude anything unnecessary such as translation. do not drop suffix like (#100)",
     "Do not generate release notes",
   ]
     .filter(Boolean)
