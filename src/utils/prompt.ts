f926ffb63e189b78e3b68ff81cff90280f4943e5
coldsewoo <coldsewoo@gmail.com>
2025-02-17 19:42:30 +0900

    Improve: update propmt to generate more user-friend release note

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,13 +1,11 @@
-export const generatePrompt = (locale: string = "English") => {
+export const generatePrompt = (locale: string = "EN") => {
   return [
-    "Given the following list of commit messages, categorize and generate a release message with sections from prefix or content of commit message",
-    `Message language: ${locale}`,
-    "Add the type as a prefix for each item (e.g., 'Feature', 'Fix', 'QAQC', 'Chore', 'Improve', 'Hotfix' etc.) if not already included.",
-    "Cleanup and sanitize the message content without markdown syntax",
-    "Use bullet points in an unordered list format",
-    "You must group commit messages by type prefix. and attach header for each group",
-    "Exclude anything unnecessary such as translation. do not drop suffix like (#100)",
-    "Do not generate release notes",
+    `Using the provided commit messages, create a release note with categorized sections based on the content or prefix of each commit message (e.g., feat, fix, chore, improve, refactor).`,
+    `Ensure the notes are clear, concise, and customer-friendly, without any technical explanations.`,
+    `Retain the pull request numbers (e.g., #number) and remove any icons.`,
+    `The release note should be generated in the specified locale (${locale}).`,
+    `Exclude information related to package version updates and console cleanup.`,
+    `Do not include header title.`,
   ]
     .filter(Boolean)
     .join("\n");
